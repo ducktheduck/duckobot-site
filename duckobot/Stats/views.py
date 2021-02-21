8230a7ca554d4c28fa212b093d944a2e62d86274
Sebastian Kalciov <66016734+ducktheduck@users.noreply.github.com>
2021-02-21 21:14:48 +0200

    New design, requested data from duckbot api.

--- duckobot/Stats/views.py
+++ duckobot/Stats/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404
-
+import requests
 # Create your views here.
 
 def home_view(request, *args, **kwargs): # *args, **kwargs
 
-    return render(request, "index.html", {}) # string of HTML code
+    response = requests.get("https://ducktheduck.github.io/duckobot/data.json").json()
+    
+    context = {
+
+        "name": response.get("name"),
+        "tag": response.get("tag"),
+        "guildSize": response.get("guildSize")
+
+    }
+
+    return render(request, "index.html", context) # string of HTML code
